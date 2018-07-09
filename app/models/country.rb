a78e69008e1de991e34782e8793e982347b496f1
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 16:26:26 -0700

    added validation to country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,2 +1,6 @@
 class Country < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :alpha2, presence: true, uniqueness: true, length: { is: 2 }
+  validates :alpha3, presence: true, uniqueness: true, length: { is: 3 }
+  validates :numeric, presence: true, uniqueness: true, length: { is: 3 }
 end
