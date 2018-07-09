b7996b858a05b24149133ce89cd7465e5aa447f5
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 15:28:21 -0700

    set custom Devise sign in logic with jwt

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -9,19 +9,29 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
   # end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by(email: sign_in_params[:email])
+    if user && user.valid_password?(sign_in_params[:password])
+      sign_in(:user, user) # Update sign in record with Devise method.
+      # Create token with JWT - 'lib/jwt_auth
+      user = { id: user.id, email: user.email }
+      token = JwtAuth.encode_token({ user: user })
+      render json: { token: token, user: user }, status: 200
+    else
+      render json: { errors: "Email or Password does not match." }, status: 500
+    end
+  end
 
   # DELETE /resource/sign_out
   # def destroy
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+  def configure_sign_in_params
+    # Use Custom Parameter Sanitizer set up in 'app/lib/devise/user' for resource correction.
+    Devise::UserParameterSanitizer.new(User, :user, params)
+  end
 end
