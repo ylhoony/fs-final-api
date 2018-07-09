13f753f5fcf847f2d89f6f8ada486779291bd607
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 15:47:14 -0700

    added first_name and last_name for rendering

--- app/controllers/api/v1/users/sessions_controller.rb
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Api::V1::Users::SessionsController < Devise::SessionsController
-  before_action :configure_sign_in_params, only: [:create]
+  # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -14,7 +14,7 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
     if user && user.valid_password?(sign_in_params[:password])
       sign_in(:user, user) # Update sign in record with Devise method.
       # Create token with JWT - 'lib/jwt_auth
-      user = { id: user.id, email: user.email }
+      user = { id: user.id, email: user.email, first_name: user.first_name, last_name: user.last_name }
       token = JwtAuth.encode_token({ user: user })
       render json: { token: token, user: user }, status: 200
     else
@@ -29,9 +29,13 @@ class Api::V1::Users::SessionsController < Devise::SessionsController
 
   protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_in_params
+  def devise_parameter_sanitizer
     # Use Custom Parameter Sanitizer set up in 'app/lib/devise/user' for resource correction.
-    Devise::UserParameterSanitizer.new(User, :user, params)
+    Devise::UserParameterSanitizer.new(User, :user, params) 
   end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
 end
