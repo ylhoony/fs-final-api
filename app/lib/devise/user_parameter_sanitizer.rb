47df05c351b92fc32484a045823ba90f0f3751bc
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 15:47:38 -0700

    added first_name and last_name to devise parameter sanitizer

--- app/lib/devise/user_parameter_sanitizer.rb
+++ app/lib/devise/user_parameter_sanitizer.rb
@@ -1,6 +1,6 @@
 class Devise::UserParameterSanitizer < Devise::ParameterSanitizer
   def initialize(*)
     super
-    permit(:sign_up, keys: [])
+    permit(:sign_up, keys: [:first_name, :last_name])
   end
 end
