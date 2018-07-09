279eddfdc7473f3cb2806bb928d2f8c40f1089d5
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 16:34:37 -0700

    generated Countries Controller

---
+++ app/controllers/api/v1/admin/countries_controller.rb
@@ -0,0 +1,34 @@
+class Api::V1::Admin::CountriesController < API::V1::ApiController
+
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def set_country
+    
+  end
+
+  def country_params
+    
+  end
+
+end
