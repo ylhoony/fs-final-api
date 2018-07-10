82d982e2f48a5952ae52dff8a14ad839a7b3e79b
Hoon Lee <yl.hoony@gmail.com>
2018-07-10 02:46:06 -0700

    set companies route methods

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,2 +1,33 @@
-class Api::V1::CompaniesController < ApplicationController
+class Api::V1::CompaniesController < Api::V1::ApiController
+
+  def index
+
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def set_company
+    
+  end
+
+  def company_params
+    
+  end
+
 end
