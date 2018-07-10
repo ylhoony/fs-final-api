224420070b305828a7a21358487829e014b6e4b1
Hoon Lee <yl.hoony@gmail.com>
2018-07-10 16:52:35 -0700

    added validations

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,4 +1,10 @@
 class Company < ApplicationRecord
+  belongs_to :country
+  belongs_to :currency
   has_many :employees
   has_many :users, through: :employees
+
+  validates :name, presence: true
+  validates :country_id, presence: true
+  validates :currency_id, presence: true
 end
