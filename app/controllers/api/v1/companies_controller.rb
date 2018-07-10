3ea50c35a05c01bee152ab3ef1e99eb351cf6d3c
Hoon Lee <yl.hoony@gmail.com>
2018-07-10 16:52:49 -0700

    set companies controller logic

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,19 +1,31 @@
 class Api::V1::CompaniesController < Api::V1::ApiController
+  before_action :set_company, only: [:show, :update, :destory]
 
   def index
-
+    companies = current_user.companies.all
+    render json: companies, status: 200
   end
 
   def create
-    
+    company = current_user.companies.create(company_params)
+    if company.persisted?
+      current_user.employees.find(company[:id]).update(subscriber: true)
+      render json: company, status: 200
+    else
+      render json: company.errors
+    end
   end
 
   def show
-    
+    render json: @company, status: 200
   end
 
   def update
-    
+    if @company.update(company_params)
+      render json: @company
+    else
+      render json: @company.errors
+    end
   end
 
   def destroy
@@ -23,11 +35,10 @@ class Api::V1::CompaniesController < Api::V1::ApiController
   protected
 
   def set_company
-    
+    @company = current_user.companies.find(params[:id])
   end
 
   def company_params
-    
+    params.require(:company).permit(:name, :dba, :street1, :street2, :city, :state, :country_id, :postal_code, :currency_id)
   end
-
 end
