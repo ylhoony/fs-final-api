97d2136a78915ae56b7ef0f698a28a137b840078
Hoon Lee <yl.hoony@gmail.com>
2018-07-09 18:29:18 -0700

    added seed currency seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,12 @@ users = User.create([ { email: "no1@no.co", password: "nonono", password_confirm
                       { email: "no3@no.co", password: "nonono", password_confirmation: "nonono", first_name: "Paul", last_name: "Trump" } ])
 
 
-countries = Country.create([{ name: "Canada", alpha2: "CA"  ,alpha3: "CAN" , numeric: "124" , active: true },
-                            { name: "Untied States", alpha2: "US"  ,alpha3: "USA" , numeric: "840" , active: true },
-                            { name: "South Korea", alpha2: "KR"  ,alpha3: "KOR" , numeric: "410" , active: true },
-                            { name: "Wonderland", alpha2: "WL"  ,alpha3: "WDL" , numeric: "999" , active: false } ])
+countries = Country.create([{ name: "Canada", alpha2: "CA", alpha3: "CAN", numeric: "124", active: true },
+                            { name: "Untied States", alpha2: "US", alpha3: "USA", numeric: "840", active: true },
+                            { name: "South Korea", alpha2: "KR", alpha3: "KOR", numeric: "410", active: true },
+                            { name: "Wonderland", alpha2: "WL", alpha3: "WDL", numeric: "999", active: false } ])
+
+currencies = Currency.create([{ name: "Canadian Dollar", alpha3: "CAD", numeric: "124", active: true },
+                              { name: "US Dollar", alpha3: "USD", numeric: "840", active: true },
+                              { name: "South Korean Won", alpha3: "KRW", numeric: "410", active: true },
+                              { name: "Wonderland Wonder", alpha3: "WDR", numeric: "999", active: false } ])
