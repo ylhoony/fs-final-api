95fa6d4c9dc65bd4f3cc53b59be81730b424f0e9
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 16:55:22 -0700

    added empty routes methods

--- app/controllers/api/v1/company_addresses_controller.rb
+++ app/controllers/api/v1/company_addresses_controller.rb
@@ -1,2 +1,33 @@
-class Api::V1::CompanyAddressesController < ApplicationController
+class Api::V1::CompanyAddressesController < Api::V1::ApiController
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def set_company_address
+    
+  end
+
+  def company_address_params
+    
+  end
+
 end
