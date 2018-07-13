2cbaa58864cf3c47f10f0adc78d51029f650ed5d
Hoon Lee <yl.hoony@gmail.com>
2018-07-13 16:55:13 -0700

    added empty routes methods

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -1,2 +1,25 @@
 class Api::V1::EmployeesController < Api::V1::ApiController
+
+  def index
+    
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  protected
+
+  def set_employee
+
+  end
+
 end
