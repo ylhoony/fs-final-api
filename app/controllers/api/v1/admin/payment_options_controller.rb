c85b1db6f5c7d61969b172affcd37a1de4869136
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 02:34:44 -0700

    generated payment_option controller :

---
+++ app/controllers/api/v1/admin/payment_options_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::Admin::PaymentOptionsController < ApplicationController
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def set_payment_option
+    
+  end
+
+  def payment_option_params
+    
+  end
+
+end
