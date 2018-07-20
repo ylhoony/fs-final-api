dbe469abab9f6298ee6d30a098631b3536fdd84d
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 18:14:08 -0700

    opened route methods, and set index route logic

--- app/controllers/api/v1/payment_terms_controller.rb
+++ app/controllers/api/v1/payment_terms_controller.rb
@@ -1,2 +1,36 @@
-class Api::V1::PaymentTermsController < ApplicationController
+class Api::V1::PaymentTermsController < Api::V1::ApiController
+
+  def index
+    account = current_user.accounts.find_by(id: params[:current_account_id])
+    if account.present?
+      payment_terms = account.payment_terms
+      if payment_terms.present?
+        render json: payment_terms, status: 200
+      else
+        render json: { payment_terms: [] }, status: 200
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
+
+  end
+
+  def create
+    
+  end
+
+  def update
+    
+  end
+
+  protected
+
+  def payment_term_params
+
+  end
+
+  def set_payment_term
+    
+  end
+
 end
