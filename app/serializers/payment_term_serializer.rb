67ea0d94bd5d09ec492da3ed17d84ceed334c4c2
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 18:16:54 -0700

    removed comment lines

--- app/serializers/payment_term_serializer.rb
+++ app/serializers/payment_term_serializer.rb
@@ -1,5 +1,4 @@
 class PaymentTermSerializer < ActiveModel::Serializer
-  # belongs_to :account
   belongs_to :payment_option
   attributes :id, :name, :days, :trade_credit_days, :trade_credit_rate, :active
 end
