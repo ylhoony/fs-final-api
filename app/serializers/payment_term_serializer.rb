0d9b9f592e9018dff86b40341698c6b1410998fb
Hoon Lee <yl.hoony@gmail.com>
2018-07-19 18:15:55 -0700

    generated file, and opened fields

---
+++ app/serializers/payment_term_serializer.rb
@@ -0,0 +1,5 @@
+class PaymentTermSerializer < ActiveModel::Serializer
+  # belongs_to :account
+  belongs_to :payment_option
+  attributes :id, :name, :days, :trade_credit_days, :trade_credit_rate, :active
+end
