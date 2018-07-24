357322749ef0654795eeef7763d3b5f77bdca420
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 17:07:28 -0700

    updated file name to accounts for creating company model

--- db/migrate/20180710014743_create_accounts.rb
+++ db/migrate/20180710014743_create_accounts.rb
@@ -1,6 +1,6 @@
-class CreateCompanies < ActiveRecord::Migration[5.2]
+class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
-    create_table :companies do |t|
+    create_table :accounts do |t|
       t.string :name, null: false
       t.string :dba
       t.string :street1
