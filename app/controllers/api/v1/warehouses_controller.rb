34ea652b02727a32fc0900cc7774f42fe5e11ae9
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 18:18:09 -0700

    opened route methods, and set warehouse_params

--- app/controllers/api/v1/warehouses_controller.rb
+++ app/controllers/api/v1/warehouses_controller.rb
@@ -1,2 +1,49 @@
-class Api::V1::WarehousesController < ApplicationController
+class Api::V1::WarehousesController < Api::V1::ApiController
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def set_warehosue
+    
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(
+      :account_id,
+      :name,
+      :contact,
+      :street1,
+      :street2,
+      :city,
+      :state,
+      :country_id,
+      :postal_code,
+      :email,
+      :phone,
+      :fax,
+      :active
+    )
+  end
+
 end
+
+
