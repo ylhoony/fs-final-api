010edbf7202e0150d44c4181dce8d53e927ba7e3
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 17:53:19 -0700

    opened routes methods

--- app/controllers/api/v1/companies/customers_controller.rb
+++ app/controllers/api/v1/companies/customers_controller.rb
@@ -1,2 +1,45 @@
-class Api::V1::Companies::CustomersController < ApplicationController
+class Api::V1::Companies::CustomersController < Api::V1::ApiController
+
+  def index
+    binding.pry
+    
+
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+
+  protected
+
+  def set_customer
+    
+  end
+
+  def customer_params
+    params.require(:customer).permit(
+      :account_id,
+      :name,
+      :tax_id,
+      :comment,
+      :warehouse_id,
+      :payment_term_id,
+      :currency_id,
+      :active
+    )
+  end
+
 end
--- app/controllers/api/v1/companies/suppliers_controller.rb
+++ app/controllers/api/v1/companies/suppliers_controller.rb
@@ -1,2 +1,25 @@
-class Api::V1::Companies::SuppliersController < ApplicationController
+class Api::V1::Companies::SuppliersController < Api::V1::ApiController
+
+  def index
+    binding.pry
+
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
 end
