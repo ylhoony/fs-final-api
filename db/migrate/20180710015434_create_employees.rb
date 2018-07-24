64ff1fbc56e74b4eedc9919a57d4da3eece3bb13
Hoon Lee <yl.hoony@gmail.com>
2018-07-23 17:06:26 -0700

    updated migration file to remove duplicates on company

--- db/migrate/20180710015434_create_employees.rb
+++ db/migrate/20180710015434_create_employees.rb
@@ -2,7 +2,7 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
   def change
     create_table :employees do |t|
       t.integer :user_id
-      t.integer :company_id
+      t.integer :account_id
       t.boolean :subscriber, default: false
 
       t.timestamps null: false
--- db/migrate/20180713235807_rename_company_table_to_account.rb
+++
@@ -1,6 +0,0 @@
-class RenameCompanyTableToAccount < ActiveRecord::Migration[5.2]
-  def change
-    rename_table :companies, :accounts
-    rename_column :employees, :company_id, :account_id
-  end
-end
