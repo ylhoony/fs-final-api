424ee9d9c91400fa27a057cc8a290c55c64a6a51
Hoon Lee <yl.hoony@gmail.com>
2018-07-26 15:26:32 -0700

    set logic for account contact controllers

--- app/controllers/api/v1/account_contacts_controller.rb
+++ app/controllers/api/v1/account_contacts_controller.rb
@@ -1,20 +1,56 @@
-class Api::V1::AccountContactsController < ApplicationController
-
+class Api::V1::AccountContactsController < Api::V1::ApiController
+  before_action :set_account
 
   def index
-    
+    if @account.present?
+      contacts = @account.contacts
+      if contacts.present?
+        render json: contacts, status: 200
+      else
+        render json: { contacts: [] }, status: 200
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def create
-    
+    if @account.present?
+      contact = @account.contacts.create(contact_params)
+      if contact.persisted?
+        render json: contact, status: 201
+      else
+        render json: contact.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def show
-    
+    if @account.present?
+      contact = @account.contacts.find(params[:id])
+      if contact.persisted?
+        render json: contact, status: 201
+      else
+        render json: contact.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def update
-    
+    if @account.present?
+      contact = @account.contacts.find(params[:id])
+      if contact.update(contact_params)
+        render json: contact, status: 201
+      else
+        render json: contact.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def destroy
@@ -24,7 +60,20 @@ class Api::V1::AccountContactsController < ApplicationController
   protected
 
   def contact_params
-    
+    params.require(:contact).permit(
+      :account_id,
+      :first_name,
+      :last_name,
+      :job_title,
+      :email,
+      :phone,
+      :mobile,
+      :fax,
+      :comment,
+      :active
+    )
   end
 
 end
+
+
