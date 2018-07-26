141fdd83c9ca7a18194789437206525f889c5b66
Hoon Lee <yl.hoony@gmail.com>
2018-07-26 11:47:59 -0700

    genereated account_contacts controller, opened routes

---
+++ app/controllers/api/v1/account_contacts_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::AccountContactsController < ApplicationController
+
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def contact_params
+    
+  end
+
+end
