b659648282e74c59dba75e4eb44f6b92fbf9d084
Hoon Lee <yl.hoony@gmail.com>
2018-07-26 11:17:07 -0700

    set association with country, and optional mode with account/company

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,6 @@
 class Address < ApplicationRecord
-  belongs_to :account
-  belongs_to :customer, class_name: "Company"
-  belongs_to :supplier, class_name: "Company"
+  belongs_to :account, optional: true
+  belongs_to :country
+  belongs_to :customer, class_name: "Company", foreign_key: "company_id", optional: true
+  belongs_to :supplier, class_name: "Company", foreign_key: "company_id", optional: true
 end
