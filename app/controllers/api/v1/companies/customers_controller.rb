8cd5f9407e753b800ba741593fcc5bc76e404524
Hoon Lee <yl.hoony@gmail.com>
2018-07-27 13:50:06 -0700

    set logics for customers routes

--- app/controllers/api/v1/companies/customers_controller.rb
+++ app/controllers/api/v1/companies/customers_controller.rb
@@ -2,19 +2,55 @@ class Api::V1::Companies::CustomersController < Api::V1::ApiController
   before_action :set_account
 
   def index
-    # binding.pry    
+    if @account.present?
+      customers = @account.customers
+      if customers.present?
+        render json: customers, status: 200
+      else
+        render json: { customers: [] }, status: 200
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def create
-    
+    if @account.present?
+      customer = @account.customers.create(customer_params)
+      if customer.persisted?
+        render json: customer, status: 201
+      else
+        render json: customer.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def show
-    
+    if @account.present?
+      customer = @account.customers.find(params[:id])
+      if customer.persisted?
+        render json: customer, status: 201
+      else
+        render json: customer.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def update
-    
+    if @account.present?
+      customer = @account.customers.find(params[:id])
+      if customer.update(customer_params)
+        render json: customer, status: 201
+      else
+        render json: customer.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def destroy
@@ -23,7 +59,7 @@ class Api::V1::Companies::CustomersController < Api::V1::ApiController
 
   protected
 
-  def supplier_params
+  def customer_params
     params.require(:customer).permit(
       :account_id,
       :type,
