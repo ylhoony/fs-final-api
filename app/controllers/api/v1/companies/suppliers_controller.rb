2c6511336567c1a4cdd54e0920d5045e32d77284
Hoon Lee <yl.hoony@gmail.com>
2018-07-27 13:50:22 -0700

    copied logic from customers and adjusted for suppliers

--- app/controllers/api/v1/companies/suppliers_controller.rb
+++ app/controllers/api/v1/companies/suppliers_controller.rb
@@ -2,19 +2,55 @@ class Api::V1::Companies::SuppliersController < Api::V1::ApiController
   before_action :set_account
 
   def index
-    # binding.pry
+    if @account.present?
+      suppliers = @account.suppliers
+      if suppliers.present?
+        render json: suppliers, status: 200
+      else
+        render json: { suppliers: [] }, status: 200
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def create
-    
+    if @account.present?
+      supplier = @account.suppliers.create(supplier_params)
+      if supplier.persisted?
+        render json: supplier, status: 201
+      else
+        render json: supplier.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def show
-    
+    if @account.present?
+      supplier = @account.suppliers.find(params[:id])
+      if supplier.persisted?
+        render json: supplier, status: 201
+      else
+        render json: supplier.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def update
-    
+    if @account.present?
+      supplier = @account.suppliers.find(params[:id])
+      if supplier.update(supplier_params)
+        render json: supplier, status: 201
+      else
+        render json: supplier.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def destroy
