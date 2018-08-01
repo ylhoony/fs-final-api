fcc7675ea55a241b818f35dde9bef96b024026fc
Hoon Lee <yl.hoony@gmail.com>
2018-08-01 15:24:09 -0700

    added addresses_attributes and contacts_attributes

--- app/controllers/api/v1/companies/customers_controller.rb
+++ app/controllers/api/v1/companies/customers_controller.rb
@@ -68,8 +68,36 @@ class Api::V1::Companies::CustomersController < Api::V1::ApiController
       :comment,
       :warehouse_id,
       :payment_term_id,
-      :currency_id
+      :currency_id,
+      addresses_attributes: [
+        :id,
+        :company_name,
+        :contact,
+        :street1,
+        :street2,
+        :city,
+        :state,
+        :country_id,
+        :postal_code,
+        :email,
+        :phone,
+        :fax,
+        :active,
+        :_destroy
+      ],
+      contacts_attributes: [
+        :id,
+        :first_name,
+        :last_name,
+        :job_title,
+        :email,
+        :phone,
+        :mobile,
+        :fax,
+        :comment,
+        :active,
+        :_destroy,
+      ]
     )
   end
-
 end
