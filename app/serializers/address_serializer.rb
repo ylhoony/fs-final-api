090d929a54439d1d4f37d3885e9233c5f03f7513
Hoon Lee <yl.hoony@gmail.com>
2018-08-01 15:23:30 -0700

    updated to return country_id, instead of details.

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,10 +1,9 @@
 class AddressSerializer < ActiveModel::Serializer
   belongs_to :account
-  belongs_to :country
   belongs_to :customer, class_name: "Company", foreign_key: "company_id"
   belongs_to :supplier, class_name: "Company", foreign_key: "company_id"
   
   attributes :id, :company_name, :contact,
-            :street1, :street2, :city, :state, :postal_code,
+            :street1, :street2, :city, :state, :postal_code, :country_id,
             :email, :phone, :fax, :comment, :active
 end
