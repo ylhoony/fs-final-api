d45346991f9997aa9e84f3762bffbcf51d597910
Hoon Lee <yl.hoony@gmail.com>
2018-08-02 17:03:17 -0700

    created product categories controller

---
+++ app/controllers/api/v1/products/product_categories_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::Products::ProductCategoriesController < Api::V1::ApiController
+
+  def index
+
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def product_category_params
+    params.require(:product_cateogry).permit(:account_id, :name, :active)
+  end
+
+end
---
+++ test/controllers/api/v1/products/product_categories_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class Api::V1::Products::ProductCategoriesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
