20e069599cef143749b1debc24767342d2b0c960
Hoon Lee <yl.hoony@gmail.com>
2018-08-07 16:29:46 -0700

    set route logics

--- app/controllers/api/v1/products/products_controller.rb
+++ app/controllers/api/v1/products/products_controller.rb
@@ -1,30 +1,75 @@
-class Api::V1::Products::ProductsController < ApplicationController
-
+class Api::V1::Products::ProductsController < Api::V1::ApiController
+  before_action :set_account
 
   def index
-    
+    if @account.present?
+      products = @account.products
+      if products.present?
+        render json: products, status: 200
+      else
+        render json: { products: [] }, status: 200
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def create
-    
+    if @account.present?
+      product = @account.products.create(product_params)
+      if product.persisted?
+        render json: product, status: 201
+      else
+        render json: product.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def show
-    
+    if @account.present?
+      product = @account.products.find(params[:id])
+      if product.persisted?
+        render json: product, status: 201
+      else
+        render json: product.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def update
-    
+    if @account.present?
+      product = @account.products.find(params[:id])
+      if product.update(product_params)
+        render json: product, status: 201
+      else
+        render json: product.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   def destroy
-    
+    if @account.present?
+      product = @account.products.find(params[:id])
+      if product.destroy
+        render json: product, status: 201
+      else
+        render json: product.errors, status: 400
+      end
+    else
+      render render json: { errors: [ message: "Access Denied" ] }, status: :unauthorized
+    end
   end
 
   protected
 
   def product_params
-    
+    # params.require(:product).permit()
   end
 
 end
