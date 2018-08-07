cc9bce3c7813ad806ae6c963bc9d5565b2e1b7a0
Hoon Lee <yl.hoony@gmail.com>
2018-08-07 15:12:36 -0700

    generated product controller

---
+++ app/controllers/api/v1/products/products_controller.rb
@@ -0,0 +1,30 @@
+class Api::V1::Products::ProductsController < ApplicationController
+
+
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  protected
+
+  def product_params
+    
+  end
+
+end
