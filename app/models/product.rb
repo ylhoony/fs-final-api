03726210eecc78d72e82534f9cf815b9a9f27236
Hoon Lee <yl.hoony@gmail.com>
2018-08-08 15:37:44 -0700

    set validation for sku and name

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :account
   belongs_to :product_brand
   belongs_to :product_category
+
+  validates :sku, presence: true, uniqueness: true
+  validates :name, presence: true
 end
