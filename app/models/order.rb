626a33b31212fc8128f052152f7f7528fbd3b0c8
Hoon Lee <yl.hoony@gmail.com>
2018-08-09 14:51:10 -0700

    set scope and associtaion

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,18 @@
 class Order < ApplicationRecord
+  scope :sales_orders, -> { where(type: 'SalesOrder')}
+  scope :purchase_orders, -> { where(type: 'PurchaseOrder')}
+  
+  belongs_to :account
+  belongs_to :account_address, class_name: "Address"
+  belongs_to :account_contact, class_name: "Contact"
+
+  belongs_to :customer, class_name: "Company", optional: true
+  belongs_to :supplier, class_name: "Company", optional: true
+  belongs_to :billing_address, class_name: "Address"
+  belongs_to :shipping_address, class_name: "Address"
+
+  belongs_to :warehouse
+  belongs_to :currency
+
+
 end
