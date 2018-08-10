d07bc99b53e092121d20f1a091d54e8b71c37fed
Hoon Lee <yl.hoony@gmail.com>
2018-08-10 14:03:04 -0700

    set serializer

---
+++ app/serializers/order_serializer.rb
@@ -0,0 +1,16 @@
+class OrderSerializer < ActiveModel::Serializer  
+  belongs_to :account
+  belongs_to :account_address, class_name: "Address"
+  belongs_to :account_contact, class_name: "Contact"
+
+  belongs_to :customer, class_name: "Company", optional: true
+  belongs_to :supplier, class_name: "Company", optional: true
+  belongs_to :billing_address, class_name: "Address"
+  belongs_to :shipping_address, class_name: "Address"
+
+  belongs_to :warehouse
+  belongs_to :currency
+  belongs_to :payment_term
+
+  attributes :id, :type, :order_reference, :comment, :order_date
+end
