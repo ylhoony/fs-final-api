2ebf13fb1606aff7ab433a87fd578f68d30cb43d
Hoon Lee <yl.hoony@gmail.com>
2018-08-09 17:01:45 -0700

    added addresses and contacts to account serializer

--- app/serializers/account_serializer.rb
+++ app/serializers/account_serializer.rb
@@ -1,6 +1,8 @@
 class AccountSerializer < ActiveModel::Serializer
   belongs_to :country
   belongs_to :currency
+  has_many :addresses
+  has_many :contacts
 
   attributes :id, :name, :dba, :street1, :street2, :city, :state, :postal_code
 end
